46497ade63d8f853dc278905b194ed1fe6a903ac
Jiangrong Shen <11521050@zju.edu.cn>
2016-03-20 19:58:43 +0800

    ok, csmath homework2.

--- PCA/PCA.py
+++ PCA/PCA.py
@@ -1,4 +1,8 @@
 import numpy as np
+import load_dataset
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from matplotlib.ticker import NullLocator
 
 
 def principle_component_analysis(data_set, top_nfeature=9999999):
@@ -25,3 +29,82 @@ def principle_component_analysis(data_set, top_nfeature=9999999):
     recon_data = (low_data_set * red_eig_vector.transpose()) + mean_val
 
     return low_data_set, recon_data
+
+
+def find_typical_points(low_data_set):
+    max_value = 30
+    min_value = -20
+
+    x = np.linspace(start=min_value, stop=max_value, num=5, endpoint=False)
+
+    # next we find some key points
+    arg_index = []
+    # we need to visualize 25 images
+    point_location = []
+    for i in range(5):
+        for j in range(5):
+            point_location.append((x[i], x[j]))   # generate 25 points
+
+    # 2. find the key points
+    for i in range(len(point_location)):
+        point = point_location[i]
+        shortest_distance = 99999999
+        arg = 0
+        for j in range(np.shape(low_data_set)[0]):
+            distance = dist_points(point[0], point[1], low_data_set[j, 0], low_data_set[j, 1])
+            if distance < shortest_distance:
+                shortest_distance = distance
+                arg = j
+        arg_index.append(arg)
+
+    return arg_index
+
+
+def dist_points(x1, y1, x2, y2):
+    return ((x1-x2)**2 + (y1-y2)**2) ** 0.5
+
+
+def test_on_PCA():
+
+    data_without_label, data_set = load_dataset.data_processing()
+
+    low_data_set, recon_data = principle_component_analysis(data_without_label, top_nfeature=2)
+
+    plt.plot(low_data_set[:, 0], low_data_set[:, 1], 'bo')
+    plt.xlabel("x")
+    plt.ylabel("y")
+    text = "PCA"
+    plt.text(4.5, 0.5, text)
+    plt.title("Principle Component Analysis")
+    plt.grid(True)   # add grid to the picture
+
+    # draw the images of these key points
+    arg_index = find_typical_points(low_data_set)
+
+    # major_locator = NullLocator()
+    # plot the points nearest these points
+    for i in range(25):
+        plt.plot(low_data_set[arg_index[i], 0], low_data_set[arg_index[i], 1], 'ro')
+    # to make the ticks not show
+    # fig, ax = plt.subplots()
+    # plt.plot(...)
+    # ax.xaxis.set_major_locator(major_locator)
+    plt.show()
+
+    # # draw the picture of 3
+    # index = 1  # just draw the reconstruction image of red point
+    # image = recon_data[index, :].reshape((8, 8))
+    # plt.imshow(image, cmap=cm.gray_r)
+    # plt.show()
+
+    k = 0
+    for i in range(5):
+        for j in range(5):
+            k += 1
+            plt.subplot(5, 5, k)
+            image = recon_data[arg_index[5 * i + j], :].reshape((8, 8))
+            plt.imshow(image, cmap=cm.gray_r)
+    plt.show()
+
+if __name__ == "__main__":
+    test_on_PCA()
--- PCA/load_dataset.py
+++ PCA/load_dataset.py
@@ -1,5 +1,100 @@
 import numpy as np
+import cPickle
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import cv2
 
 
-def load_dataset():
-    pass
+def load_dataset(file_name):
+    # This method wastes much memory.
+    # file = open("optdigits.tes")
+    file = open(file_name)
+    result = []
+
+    while 1:
+        line = file.readline()
+
+        if not line:
+            break
+        # note that the final string is "\n", not the label of the image
+        if line[len(line)-2] == "3":
+            result.append(map(int, line.split(",")))
+    file.close()
+
+    # cPickle.dump(result, open("data_tes", "wb"))
+    cPickle.dump(result, open("data_" + file_name, "wb"))
+
+
+def data_processing():
+
+    load_dataset(file_name="optdigits.tra")
+    load_dataset(file_name="optdigits.tes")
+
+    # read data from the data file
+    data_tra = cPickle.load(open("data_optdigits.tra", "rb"))
+    data_tes = cPickle.load(open("data_optdigits.tes", "rb"))
+    data = np.vstack((data_tra, data_tes))
+
+    data_without_label = np.zeros((len(data), 64))
+    data_without_label = data[:, :-1]
+    # To save the original data set, we must use the "np.copy", that is called deep copy.
+    # if you use the "a = b", then it is only copy the point, which means shallow copy.
+    data_without_label_origin = np.copy(data_without_label)
+    # print np.shape(data_without_label)
+
+    # Next we reshape all images to (8*8)
+    all_image_reshape = np.zeros((np.shape(data_without_label)[0], 8, 8))
+    # print data_without_label[1, :]
+    for i in range(np.shape(data_without_label)[0]):
+        data_without_label[i, np.argwhere(data_without_label[i, :] != 0)] = 1
+        all_image_reshape[i, :, :] = data_without_label[i, :].reshape((8, 8))
+
+    # print all_image_reshape[1, :, :]
+    return data_without_label_origin, all_image_reshape
+
+
+if __name__ == "__main__":
+
+    data_without_label, all_image_reshape = data_processing()
+
+    # Just test the image
+    image = data_without_label[3, :].reshape((8, 8))
+
+    plt.imshow(image, cmap=cm.gray_r)
+    plt.show()
+
+    for i in range(8):
+        for j in range(8):
+            if image[i][j] > 0:
+                image[i][j] = 1
+    # print image
+
+    # use the matplotlib method show the image
+    # note here I add the parameter "cmap=cm.gray_r" to make it show the gray image better
+    plt.imshow(image, cmap=cm.gray_r)
+    plt.show()
+
+    # # use the opencv method to show the images
+    # # win = cv2.namedWindow('test win', flags=0)
+    # # cv2.imshow('test win', image)
+    # # cv2.waitKey(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
